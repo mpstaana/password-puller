253faf0e7d59f478b2f6f5fa69483b2ecc82d62b
Miguel Sta Ana <48932429+mpstaana@users.noreply.github.com>
2019-07-10 05:43:52 +0800

    Add files via upload

---
+++ pw.py
@@ -0,0 +1,20 @@
+#! python3
+# pw.py - a basic password locker program
+
+PASSWORDS = {'facebook': #password,
+             'twitter': #password,
+             'instagram': #password}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
+        
